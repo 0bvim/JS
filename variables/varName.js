afb8ff280af7adfadfddc4e433375c3dfa7dd08d
Vinicius Pereira <niviciusdev@gmail.com>
2023-08-03 19:40:47 -0300

    feat: good variables names  Changes to be committed: 	new file:   variables/varName.js

---
+++ variables/varName.js
@@ -0,0 +1,13 @@
+//Create variable with good names. String e.g.:
+const	fullName = "Jhon Wicked";
+const	city	= "Angel's City";
+
+//Name for variables that work with numbers
+const age = 30;
+const height = 1.75;
+
+
+console.log(fullName);
+console.log(city);
+console.log(age);
+console.log(height);
