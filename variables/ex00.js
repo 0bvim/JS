6efe87455bf8288ac69e1e0023b490275c37f2bf
Vinicius Pereira <niviciusdev@gmail.com>
2023-08-03 19:31:55 -0300

    feat: ex00  Changes to be committed: 	new file:   variables/ex00.js

---
+++ variables/ex00.js
@@ -0,0 +1,9 @@
+const	myName = "Thalia";
+const	birthCity = "São Paulo";
+const	birthYear = 2020;
+
+console.log("My name is " + myName + ",", "I borned in " + birthYear + ", in", birthCity + ".");
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
