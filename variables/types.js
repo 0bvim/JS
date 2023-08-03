4b734cefbb1fb996596b4bf5f364dc15897acdcc
Vinicius Pereira <niviciusdev@gmail.com>
2023-08-03 19:03:17 -0300

    feat: working with types  Changes to be committed: 	new file:   types.js
     Changes not staged for commit:
    	deleted:    ../types.js

---
+++ variables/types.js
@@ -0,0 +1,20 @@
+let	patientId = 50;
+let	isEnrolled = true;
+let	patientEmail = 'ana@email.com';
+
+/*example 1:
+ typeof shows type of variable when you put it in console.log
+	*/
+console.log(typeof patientId);
+
+/*
+ when you have a variable not declared, type will be 'undefined'.
+ example 2: */
+console.log(typeof patientAge);
+
+/*
+ changing variable type from int to string
+ example 3:
+ */
+let	patientId_2 = '50';
+console.log(typeof patientId_2);
